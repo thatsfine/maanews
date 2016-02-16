2dd99aef820cb6f8b70670fedeb8b9e407233f5e
cs laptop <cslaptop@WL-27-27.CINE.Claremont.Edu>
2016-02-16 00:13:03 -0800

    Started working on table view cells

---
+++ MathNews/MathNews/ArticleTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  ArticleTableViewCell.swift
+//  MathNews
+//
+//  Created by cs laptop on 2/15/16.
+//  Copyright (c) 2016 cs121MathNewApp. All rights reserved.
+//
+
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+    //MARK: Properties
+    
+    @IBOutlet weak var LinkLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
