1bf913677beb5afc059d4da0d4e525f5fc29a35b
madelinepignetti <mpignetti@g.hmc.edu>
2016-04-01 11:58:42 -0700

    able to scrape titles from FB

--- MathNews/MathNews/TableViewController.swift
+++ MathNews/MathNews/TableViewController.swift
@@ -14,39 +14,38 @@ class TableViewController: UITableViewController {
     //MARK: Properties
     var articles = [String]()
     var articleContent = [String]()
-    
-    func loadSampleArticles(){
-        let link1 = "Secret to the perfect pancake described mathematically"
-       
-        let content1 = "Maths students from the University of Sheffield have swapped calculus for the kitchen by developing a formula to prepare the perfect pancake."
-        let link2 = "Corals, crochet and the cosmos:how hyperbolic geometry pervades the universe"
-    
-        let content2 = " We have built a world of largely straight lines – the houses we live in, the skyscrapers we work in and the streets we drive on our daily commutes."
-        let link3 = "The Intriguing Math That Turns Manhattan Properties Into Shekels"
-        
-        let content3 = "What do Israeli investors know about retirement homes in Indiana? Enough to lend them $68 million.Strawberry Fields, a real estate investment trust whose facilities cater to Alzheimer’s sufferers in the Midwest, is the latest of at least 14 U.S. property companies that have borrowed a combined 8.1 billion shekels ($2.07 billion) in the Israeli market since 2008, securing interest rates they could only dream of at home."
-        //the titles
-        articles+=[link1,link2,link3]
-        //the content
-        articleContent+=[content1,content2,content3]
-        
-    }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
         
+        var counter = 0
+        //Create reference to database
         let ref = Firebase(url:"https://crackling-torch-4312.firebaseio.com")
         // Retrieve new posts as they are added to your database
-        ref.observeEventType(.ChildAdded, withBlock: { snapshot in
-            print(snapshot.value.objectForKey("KCIVooi4bqdPPIGZN2l"))
-            print(snapshot.value.objectForKey("title"))
+        ref.observeEventType(.Value, withBlock: { snapshot in
+           
+            //
+            for item in snapshot.children {
+                let keysArray = Array((snapshot.value as! NSDictionary).allKeys as! [String])
+                let title = snapshot.value[keysArray[counter]] as! String
+                self.articles.append(title)
+                self.articleContent.append("blurb will go here")
+                ++counter
+            }
+            self.tableView.reloadData()
         })
+       
+       
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      //  loadArticles()
 
-        //load the sample articles
-        loadSampleArticles()
 
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
