0818922a025b8d554bad254b982b788a35fc20a2
kcalderon3 <kcalderon@g.hmc.edu>
2016-04-01 11:57:20 -0700

    got blurbs now trying to update firebase schema

--- MathNews/scraper/sciAmScrape.py
+++ MathNews/scraper/sciAmScrape.py
@@ -40,16 +40,56 @@ print titles
 urList=[]
 for urls in soup.find_all(href=re.compile("article")):
     urList.append(urls['href'])
+#k
+print urList
+
+
+#Find blurbs
+blurbs = []
+for para in soup.find_all('p', {'class': 't_body listing-wide__inner__desc'}):
+	blurbs.append(para.string)
 
 
 # In[34]:
 
-for i in range(0,len(titles)):
-    result=firebase.post('/Test', titles[i])
-    print result
+# for i in range(0,len(titles)):
+#     result=firebase.post('/', titles[i])
+#     #result=firebase.post('/', urList[i])    could uncomment and alternate url after title
+#     print result
+# #since particular site lists URLs twice, print every other url using [2*j]
+# for j in range(0, len(urList)):
+# 	result=firebase.post('/', urList[2*j])
+# 	print result
+#  for k in range(0, len(blurbs)):
+#  	result=firebase.post('/', blurbs[k])
+#  	print result
 
-
+result = firebase.post('/articles', 'hi')
+print result
 # In[ ]:
+#Now for the blurb: turn each url into a soup 
+
+# for k in range(0, len(urList)):
+# 	quMath_url = urList[2*k]
+# 	page_html = urllib2.urlopen(quMath_url) 
+# 	soup = BeautifulSoup(page_html)
+# 	# Find article titles and print them
+# 	#blurbs = []
+# 	#for para in soup.find('p').getText():
+#  	#   blurbs.append(para['data-listing-title'])
+# 	#print soup.find('p').getText()
+# 	result = firebase.post('/', soup.p)
+# 	print result
+
+
+
+
+
+
+
+
+
+
 
 
 
