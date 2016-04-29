1ca4b72239f384a1856905dd87815755defe46b5
kcalderon3 <kcalderon@g.hmc.edu>
2016-04-28 20:12:17 -0700

    splash screen only use 6s

--- MathNews/MathNews/ViewController.swift
+++ MathNews/MathNews/ViewController.swift
@@ -9,9 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var splashImage: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
